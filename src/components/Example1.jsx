9cb09dcb0f2a5832588709cc73a16d86ee7f3b39
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-01-13 18:31:03 +0100

    testing

--- src/components/Example1.jsx
+++ src/components/Example1.jsx
@@ -1,11 +1,35 @@
 import React, { useState } from "react";
 
+//
+//
+// import img1 from "https://images.pexels.com/photos/10679232/pexels-photo-10679232.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+// import img2 from "https://images.pexels.com/photos/10679206/pexels-photo-10679206.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+// import img3 from "https://images.pexels.com/photos/10679204/pexels-photo-10679204.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+
 const Example1 = () => {
-  const [value, setValue] = useState(0);
+  const [myProfession, setMyProfession] = useState("");
+  //
+  //
+  const professions = ["police", "chef", "doctor"];
+  //
+  //
 
   return (
     <>
-      <section></section>
+      <div className="btn-group" role="group" aria-label="basic example">
+        {professions.map((profession) => {
+          <button type="button" key={profession} className="btn-card">
+            {profession.toLocaleUpperCase()}
+          </button>;
+        })}
+      </div>
+
+      {/* ------ */}
+
+      <div className="rendering-profession">
+        {/* once you will click, this below will render */}
+        <p>{myProfession}</p>
+      </div>
     </>
   );
 };
---
+++ src/components/data.js
@@ -0,0 +1,10 @@
+export const images = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1468488718849-422a2a5efc03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1548142813-c348350df52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80",
+  },
+];
