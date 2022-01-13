ce1042f6ae15f70398655fe13b29ce598599a22f
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-01-13 19:32:35 +0100

    testing

--- src/components/Example1.jsx
+++ src/components/Example1.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 //
 //
-// import img1 from "https://images.pexels.com/photos/10679232/pexels-photo-10679232.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+import img1 from "../img/police.jpg";
 // import img2 from "https://images.pexels.com/photos/10679206/pexels-photo-10679206.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 // import img3 from "https://images.pexels.com/photos/10679204/pexels-photo-10679204.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 
@@ -48,6 +48,12 @@ const Example1 = () => {
         <div className="rendering-profession">
           {/*4  once you will click on the button above, this below will render */}
           <p>{myProfession}</p>
+
+          <p>
+            {myProfession === "police" && <ProfessionImg src={img1} />}
+            {/* {myProfession === "chef" && <ProfessionImg src={img2} />}
+            {myProfession === "doctor" && <ProfessionImg src={img3} />} */}
+          </p>
         </div>
       </section>
     </>
