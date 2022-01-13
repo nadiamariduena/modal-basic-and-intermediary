e45e81dc92997382ec940d8621f888da6e6b26c7
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-01-13 18:48:14 +0100

    mpping the text before implmenting the images

--- README.md
+++ README.md
@@ -5,3 +5,19 @@
   openModal && <Modal />;
 }
 ```
+
+##### The toLocaleLowerCase() method does not change the original string. The toLocaleLowerCase() returns the same result as toLowerCase() , except for locales that conflict with the regular Unicode case mappings (such as Turkish).
+
+```javascript
+        {professions.map((profession) => (
+            <button
+              type="button"
+              key={profession}
+              className="btn-card"
+              onClick={() => setMyProfession(profession)}
+            >
+              {profession.toLocaleUpperCase()}
+            </button>
+          ))}
+        </div>
+```
--- src/components/Example1.jsx
+++ src/components/Example1.jsx
@@ -7,29 +7,45 @@ import React, { useState } from "react";
 // import img3 from "https://images.pexels.com/photos/10679204/pexels-photo-10679204.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 
 const Example1 = () => {
+  // 1
+  const professions = ["police", "chef", "doctor"];
+  // 2
   const [myProfession, setMyProfession] = useState("");
   //
   //
-  const professions = ["police", "chef", "doctor"];
+
   //
   //
 
   return (
     <>
-      <div className="btn-group" role="group" aria-label="basic example">
-        {professions.map((profession) => {
-          <button type="button" key={profession} className="btn-card">
-            {profession.toLocaleUpperCase()}
-          </button>;
-        })}
-      </div>
+      <section className="example2">
+        {/* 
+        
+        
+        
+        */}
+        <div className="btn-group">
+          {/* 3 */}
+          {professions.map((profession) => (
+            <button
+              type="button"
+              key={profession}
+              className="btn-card"
+              onClick={() => setMyProfession(profession)}
+            >
+              {profession.toLocaleUpperCase()}
+            </button>
+          ))}
+        </div>
 
-      {/* ------ */}
+        {/* ------ */}
 
-      <div className="rendering-profession">
-        {/* once you will click, this below will render */}
-        <p>{myProfession}</p>
-      </div>
+        <div className="rendering-profession">
+          {/*4  once you will click on the button above, this below will render */}
+          <p>{myProfession}</p>
+        </div>
+      </section>
     </>
   );
 };
