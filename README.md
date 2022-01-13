6e27fb0b91e373235c4bf283e218b06643967e60
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-01-13 22:02:15 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
- 
+## [useState Exercises | Click events]()
+
+#### Recap exercises | React 2022 🍨
+
+- The lesson related to useStates can be found here [useState | Recap](https://github.com/nadiamariduena/react-recap-2022/tree/5-useState-counter) ( **it will be available soon**)
+
+<br>
+<br>
+
+- Photography [cottonbro](https://www.pexels.com/fr-fr/@cottonbro/collections/)
+
+[<img src="./src/img/slider2-test.gif"/>](https://github.com/nadiamariduena/modal-basic-and-intermediary)
--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Example1 from "./components/Example1";
+// Video  pexels-cottonbro-10678920.mp4
 
 import Home from "./Home";
 // import Navbar from "./Navbar";
+const videoBg =
+  "https://images.pexels.com/photos/10679086/pexels-photo-10679086.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 
 function App() {
   return (
     <Router>
       {/* <Navbar /> */}
       <main className="page">
-        <div className="app">
+        <div
+          className="app"
+          style={{
+            backgroundImage: `url(${videoBg})`,
+            objectFit: "cover",
+          }}
+        >
           <Switch>
             {" "}
             <Route path="/" exact component={Home} />
