dc13839a879437f5e6c501157904f9683c5f1ec4
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-01-13 21:30:30 +0100

    readme

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,31 @@
 import React, { useState } from "react";
 import ModalContent from "./ModalContent";
+//
+import styled from "styled-components";
+
+// Video  pexels-cottonbro-10678920.mp4
+const videoBg =
+  "https://images.pexels.com/photos/10679086/pexels-photo-10679086.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
+
+const VideoWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: fixed;
+  .VideoProductContainer {
+    width: 100%;
+    height: 100%;
+    z-index: 2; //when i hid this one the icon scroll to bottom worked */
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    .video__product__box {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+`;
 
 const Home = () => {
   const [openModal, setOpenModal] = useState(false);
