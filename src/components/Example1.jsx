2b466e332a4c07ba677966cd234dcd37e159eafb
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-01-13 19:03:24 +0100

    readme

--- src/components/Example1.jsx
+++ src/components/Example1.jsx
@@ -54,4 +54,14 @@ const Example1 = () => {
   );
 };
 
+//
+// ------- Component related to the image
+
+const ProfessionImg = ({ src }) => {
+  return <img src={src} alt="" style={{ width: "200px", height: "250px" }} />;
+};
+
+//
+//
+
 export default Example1;
